4ab2cd54b6c6b4ea0f68c8040cf691e13612ea5d
RajTomar-123 <rajt70172@gmail.com>
2025-11-17 23:33:36 +0530

    Update resume download link in Header component

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -44,11 +44,8 @@ const Header = ({isDarkMode}) => {
             initial={{y: 30, opacity: 0}}
             whileInView={{y: 0, opacity: 1}}
             transition={{duration: 0.6, delay: 1}}
-<<<<<<< HEAD
+
             href='/Raj-resume.pdf' download className={`px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 ${isDarkMode ? "bg-white text-black" : ""}`}>My resume <Image src={assets.download_icon} alt='' className='w-4' /></motion.a>
-=======
-            href='/assets/public/NewResume (R).pdf' download className={`px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 ${isDarkMode ? "bg-white text-black" : ""}`}>My resume <Image src={assets.download_icon} alt='' className='w-4' /></motion.a>
->>>>>>> 8191e1805872f9fd0e087c3907407f87990b66c4
         </div>  
     </div>
   )
